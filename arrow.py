1a6dae9d4fd7b1973f60b0fa98e62900400e3f9d
tony guo <tony92151@yahoo.com.tw>
2018-12-10 22:03:35 +0800

    plot OK

--- arrow.py
+++ arrow.py
@@ -1,16 +1,51 @@
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
 import time
 
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib import pyplot as plt
-
+plt.ion()
 fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_aspect("equal")
+ax = fig.add_subplot(111, projection='3d')
 
-a = Arrow3D([0,1],[0,1],[0,1], mutation_scale=20, lw=1, arrowstyle="-|>", color="k")
-ax.add_artist(a)
-plt.show()
-time.sleep(10)
+ax.set_xlim(0, 600)
+ax.set_ylim(0, 600)
+ax.set_zlim(0, 600)
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+
+def rot_z(q):
+    r_z = np.array([[ np.cos(q), -np.sin(q), 0],
+                    [ np.sin(q),  np.cos(q), 0],
+                    [         0,          0, 1]])
+    return r_z
+
+point = np.array([[100 , 100 , 350 , 350 , 350 ],
+                  [100 , 100 , 100 , 100 , 100 ],
+                  [000 , 373 , 373 , 35  , 15  ]])
+#print(point)
+
+#point = np.dot(rot_z(np.pi/6),point)
+
+#print(point)
+for k in range(10):
+        point = np.dot(rot_z(np.pi/60),point)
+
+        X = point[0][:]
+        Y = point[1][:]
+        Z = np.array([point[2][:]])
+
+        for i in range(len(X)):
+                text = "(%d %d %d)" % (X[i],Y[i],Z[0][i])
+                ax.text(X[i]+0.1, Y[i]+0.2, Z[0][i]+0.2,text,fontsize=8) 
+        ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+        ax.scatter(X, Y, Z, c='b', marker='o')
+
+        plt.show()
+        time.sleep(0.5)
+        print(point)
+#ax.text(X, Y, Z, 'rer',fontsize=12)
+#Axes3D.text(0.5, 0.5, 0.5, s, zdir=None, **kwargs)
+plt.show(block=True)
