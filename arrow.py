0f962594e63f0c9aad82f75225ceca15bce5385e
tony guo <tony92151@yahoo.com.tw>
2019-01-05 20:33:42 +0800

    up

--- arrow.py
+++ arrow.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
 
+<<<<<<< HEAD
 #plot
+=======
+>>>>>>> 1a6dae9... plot OK
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -17,6 +20,7 @@ ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
 
+<<<<<<< HEAD
 point = np.array([[100 , 100 , 350 , 350 , 350 ],
                   [100 , 100 , 100 , 100 , 100 ],
                   [000 , 373 , 373 , 35  , 15  ]])
@@ -44,12 +48,15 @@ def init():
         theta6 = 0
 
 
+=======
+>>>>>>> 1a6dae9... plot OK
 def rot_z(q):
     r_z = np.array([[ np.cos(q), -np.sin(q), 0],
                     [ np.sin(q),  np.cos(q), 0],
                     [         0,          0, 1]])
     return r_z
 
+<<<<<<< HEAD
 
 
 Tw_0 =np.array([[               1,               0,             0,             10],
@@ -66,6 +73,14 @@ T0_1 =np.array([[  np.cos(theta1), -np.sin(theta1),             0,             1
 #########################################################################################################
 
 
+=======
+point = np.array([[100 , 100 , 350 , 350 , 350 ],
+                  [100 , 100 , 100 , 100 , 100 ],
+                  [000 , 373 , 373 , 35  , 15  ]])
+#print(point)
+
+#point = np.dot(rot_z(np.pi/6),point)
+>>>>>>> 1a6dae9... plot OK
 
 #print(point)
 for k in range(10):
@@ -80,6 +95,7 @@ for k in range(10):
                 ax.text(X[i]+0.1, Y[i]+0.2, Z[0][i]+0.2,text,fontsize=8) 
         ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
         ax.scatter(X, Y, Z, c='b', marker='o')
+<<<<<<< HEAD
 
         plt.show()
         time.sleep(0.5)
@@ -94,3 +110,12 @@ plt.show(block=True)
 class arm(dh):
         def __init__(self,dh):
                 
+=======
+
+        plt.show()
+        time.sleep(0.5)
+        print(point)
+#ax.text(X, Y, Z, 'rer',fontsize=12)
+#Axes3D.text(0.5, 0.5, 0.5, s, zdir=None, **kwargs)
+plt.show(block=True)
+>>>>>>> 1a6dae9... plot OK
